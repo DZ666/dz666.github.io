cb1983d3994f6b01ce98f6e94eeb77ba9cc2d821
Danil <danililin858@gmail.com>
2020-06-30 14:46:03 +0400

    First Commit

---
+++ precache-manifest.cb673828605934198b9bdf81c7abe048.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f76852ef13a8ba5bc680ce6b08b1615",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29669e3951a2691a8a62",
+    "url": "/static/css/2.bf0413c3.chunk.css"
+  },
+  {
+    "revision": "41f247bba4445e60af8f",
+    "url": "/static/css/main.71091496.chunk.css"
+  },
+  {
+    "revision": "29669e3951a2691a8a62",
+    "url": "/static/js/2.df221adb.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/static/js/2.df221adb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41f247bba4445e60af8f",
+    "url": "/static/js/main.19ac9349.chunk.js"
+  },
+  {
+    "revision": "1a4a63fdb153f1431e84",
+    "url": "/static/js/runtime-main.84b7232b.js"
+  },
+  {
+    "revision": "c880c33aea3e7965c555a228a1f4b13b",
+    "url": "/static/media/Baron Neue.c880c33a.otf"
+  },
+  {
+    "revision": "4dc5956a31b5832b356867433bb4e516",
+    "url": "/static/media/Myraid.4dc5956a.otf"
+  },
+  {
+    "revision": "9e08fe2424700c0b3004c62e9bdaa4c2",
+    "url": "/static/media/atmosphere.9e08fe24.png"
+  },
+  {
+    "revision": "d2938157853df238b4036450e4a2d766",
+    "url": "/static/media/dots.d2938157.png"
+  },
+  {
+    "revision": "2ea852e64630e645e87851c001ac718b",
+    "url": "/static/media/logoLarge.2ea852e6.png"
+  },
+  {
+    "revision": "c1640822ff8adb15ccab0818c211f068",
+    "url": "/static/media/planetEmpty.c1640822.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
